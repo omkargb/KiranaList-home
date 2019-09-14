03fd036003189d798f35c9a96a1dda4d496e9f9a
Omkar G B <omkargb107@gmail.com>
2019-09-14 18:55:23 +0530

    Add files via upload

---
+++ one/assets/js/vegdata_1.js
@@ -0,0 +1,115 @@
+var  vegdata = new Vue({
+el: '#vegetables',
+data: {
+ vegdata: [
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Artichoke  ",	vmn: "   ", 	vmtn:" ( Artichoke  )"	,	von: "  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Ashgourd  ",	vmn: " कोहळा  ", 	vmtn:" ( kohalaa )"	,	von: " kohalaa " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Asparagus  ",	vmn: " शतावरी ", 	vmtn:" ( Asparagus )"	,	von: "  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Beetroot  ",	vmn: " बीट ", 	vmtn:" ( Beet )"	,	von: " Chukandar " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Bitter Gourd	  ",	vmn: " कारले  ", 	vmtn:" ( Karle )"	,	von: " Karela " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Bottle Gourd	  ",	vmn: " दुधी भोपळा  ", 	vmtn:" ( Doodhi Bhopla )"	,	von: " Lauki, Kaddu " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal - black large   ",	vmn: " वांगी ( भरीतकरता  ) ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal -  black long  ",	vmn: " वांगी ( भजीकरता ) ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal - black Indian mini  ",	vmn: " वांगी ( भाजीकरता )  ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal - striped purple  ",	vmn: " वांगी  ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal -  Thai greenish ",	vmn: " वांगी  ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal - green small  ",	vmn: " वांगी  ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brinjal - purple long  ",	vmn: " वांगी  ", 	vmtn:" ( Vangi )"	,	von: " Baigan " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Brokoli ",	vmn: " ब्रोकोली  ", 	vmtn:" ( Brokoli )"	,	von: " Brokoli " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Cabbage  ",	vmn: " कोबी ", 	vmtn:" ( Kobi )"	,	von: " Pattagobi " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Capsicum - Green",	vmn: " हिरवी ढोबळी मिरची  ", 	vmtn:" ( Dhobli Mirchi )"	,	von: " Simla Mirch " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Capsicum - Red ",	vmn: " लाल ढोबळी मिरची ", 	vmtn:" ( Dhobli Mirchi )"	,	von: " Simla Mirch " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Capsicum - Yellow ",	vmn: " पिवळी ढोबळी मिरची ", 	vmtn:" ( Dhobli Mirchi )"	,	von: " Simla Mirch " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Carrot - light orange ",	vmn: " गाजर ", 	vmtn:" ( Gajar )"	,	von: " Gajar " },     
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Carrot - orange ",	vmn: " गाजर ", 	vmtn:" ( Gajar )"	,	von: " Gajar " },     
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Carrot - red  ",	vmn: " गाजर ", 	vmtn:" ( Gajar )"	,	von: " Gajar " },     
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Cauliflower  ",	vmn: " फ्लॉवर ", 	vmtn:" ( flowar )"	,	von: " Phoolgobi " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "   Cluster Beans	 ",	vmn: " गवार  ", 	vmtn:" ( Gavar )"	,	von: " Gavar " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Colacassia  ",	vmn: " अळुची पाने ", 	vmtn:" ( Alu chi pane )"	,	von: " Arvi " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Coriander leaves   ",	vmn: " कोथींबीर ", 	vmtn:" ( Kothimbir )"	,	von: " dhaniya ke patte " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Corn - Maize	  ",	vmn: " मका / कणीस ", 	vmtn:" ( Makaa / kanis )"	,	von: " Makkai / bhutta " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Cucumber - Green long ",	vmn: " काकडी  ", 	vmtn:" ( Kaakdi )"	,	von: " Kaakdi " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Cucumber - white short ",	vmn: " काकडी  ", 	vmtn:" ( Kaakdi )"	,	von: " Kaakdi " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Drumstick  ",	vmn: " शेवगा शेंग ", 	vmtn:" ( Shevaga sheng )"	,	von: "  Shingh phali" }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Field beans  ",	vmn: " वाल-पापडी  ", 	vmtn:" ( Val-papdi	 )"	,	von: "  Val-papdi " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  French/green beans  ",	vmn: " फरसबी  ", 	vmtn:" ( Farasbi )"	,	von: " Farasbi/Bakla " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Garlic ",	vmn: " लसुण  ", 	vmtn:" ( Lasun )"	,	von: " Lahasun " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Green Chili - Thin ",	vmn: " हिरवी मिरची   ", 	vmtn:" ( Hirvi Mirchi	 )"	,	von: " Hari Mirch " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Green Chili - Thick ",	vmn: " हिरवी मिरची   ", 	vmtn:" ( Hirvi Mirchi	 )"	,	von: " Hari Mirch " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Green/fresh peas  ",	vmn: " मटार / वाटाणा ", 	vmtn:" ( Matar / Vatana )"	,	von: " Matar / Vatana " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  green-sorrel	  ",	vmn: " आंबट चुका ", 	vmtn:" ( Ambat chuka )"	,	von: "  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Hyacinth beans  ",	vmn: " सुरती पापडी  ", 	vmtn:" ( surti papdi		 )"	,	von: " surti papdi" }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Ivy Gourd - Tindora   ",	vmn: " तोंडली  ", 	vmtn:" ( Tondali )"	,	von: " Tindora, Tendli " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Jackfruit - raw  ",	vmn: " कच्चा फणस  ", 	vmtn:" ( Kachcha phanas )"	,	von: " Kathal " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Knolkol  ",	vmn: " नवलकोल  ", 	vmtn:" ( Navalkol )"	,	von: " Gathgobi " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Lady Finger - Okra  ",	vmn: " भेंडी  ", 	vmtn:" ( Bhendi )"	,	von: " Bhindi " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Long Beans   ",	vmn: " चवळी शेंग  ", 	vmtn:" ( Chavli sheng )"	,	von: "  Lobia " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Mushroom  ",	vmn: " मशरूम  ", 	vmtn:" ( Mushroom )"	,	von: "  Khumb , Guchhi" }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Mustard green leaves ",	vmn: " मोहरीची पाने  ", 	vmtn:" ( Mohari chi paane )"	,	von: " Sarson ka sag " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Onion stalks  ",	vmn: " पाती कांदा ", 	vmtn:" ( Pati kanda	 )"	,	von: " Pyaz ki patti " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Onion - Brown ",	vmn: " कांदा ", 	vmtn:" ( Kanda )"	,	von: " Pyaj " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: " Onion - red  ",	vmn: " लाल कांदा  ", 	vmtn:" ( Kanda )"	,	von: " Pyaj " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "   Pearl Onion ",	vmn: " पांढरा कांदा  ", 	vmtn:" ( pandhara kanda )"	,	von: " safed pyaj " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Plantain, green  ",	vmn: " कच्ची केळी ", 	vmtn:" ( kachcha Kel )"	,	von: " Kela (hara) " }, 
+ {              imgurl: 'assets/img/desk.jpg',	 ven: "  Plantain flower  ",	vmn: " केळ फूल  ", 	vmtn:" ( Kel phool )"	,	von: " Kele-ka-phool " }, 
+
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Potato  ",	vmn: " बटाटा ", 	vmtn:"  ( Batata )"	,	von: "  Aloo " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: " Pumpkin   ",	vmn: " भोपळा  ", 	vmtn:"  ( Bhopla )"	,	von: " Kaddu  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Radish  ",	vmn: " मुळा  ", 	vmtn:"  ( Mula )"	,	von: " Muli  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Red Sorrel	  ",	vmn: " अंबाडी 	 ", 	vmtn:"  ( Ambadi )"	,	von: "  Gongura " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: " Red-leaf-spinach   ",	vmn: " माठ  ", 	vmtn:"  ( Maath )"	,	von: "   " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Ridge gourd  ",	vmn: " दोडका   ", 	vmtn:"  ( Dodka / shirale )"	,	von: " Turai   " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Snake gourd	  ",	vmn: " पडवळ  ", 	vmtn:"  ( Padval )"	,	von: " Chichinda  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "   Sponge gourd	  ",	vmn: " घोसाळी  ", 	vmtn:"  ( Ghosali )"	,	von: "   " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Sweet Potato - common ",	vmn: " रताळी  ", 	vmtn:"  ( Ratali )"	,	von: " Surti Kand  " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Sweet Potato - others ",	vmn: " रताळी  ", 	vmtn:"  ( Ratali )"	,	von: " Surti Kand  " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Tomato - red  ",	vmn: " लाल टोमॅटो  ", 	vmtn:"  ( Tomato  )"	,	von: "  Laal tamatar " }, 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Tomato - others  ",	vmn: " टोमॅटो ", 	vmtn:"  ( Tomato  )"	,	von: "  Tamatar " }, 
+ 
+ {              imgurl: 'assets/img/desk.jpg',	ven: "  Yam  ",	vmn: " सुरण  ", 	vmtn:"  ( Suran )"	,	von: "  Suran " }
+ 
+],
+
+    currentItem: {}
+
+},
+
+ methods: {
+    setCurrentItem: function(item) {
+      this.currentItem = item
+    }
+  }
+  
+})
+
+ 
+var secondTable = $('.veg-table');
+var idCounter = 0;
+$(document).on('click', "[class*=vegcopy]", function () {
+    var row = $(this).closest('li');
+    if(typeof(row.attr('clone-id')) != 'undefined') {
+    	secondTable.find('li[clone-id=\''+ row.attr('clone-id') +'\']').remove();
+      row.removeAttr('clone-id');
+    } else {
+    	row.attr('clone-id', idCounter);
+      var clone = row.clone();
+      secondTable.append(clone);
+      idCounter++;
+    }
+});
+
